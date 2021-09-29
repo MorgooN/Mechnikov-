ecdaf5dee50d2fbce64ce75dd1b61406e169763f
MorgooN <morgunov.96@bk.ru>
2021-09-29 19:42:17 +0300

    Minor chang to the basic theory, wich inclides the reading from the array

--- RevisitingArray/src/com/company/theory/DVD.java
+++ RevisitingArray/src/com/company/theory/DVD.java
@@ -2,7 +2,6 @@ package com.company.theory;
 
 
 public class DVD{
-    DVD[] dvdarray = new DVD[15];
     String name;
     String author;
     int relDate;
@@ -13,12 +12,21 @@ public class DVD{
         this.relDate = relDate;
     }
 
-    public void toSring(){
-        System.out.println(this.name + " of " + this.author + " was released " + this.relDate);
+    public String toSring(){
+        return this.name + " of " + this.author + " was released " + this.relDate;
     }
 
     public static void main(String args[]){
+        DVD[] dvdarray = new DVD[2];
         DVD dv1 = new DVD("Stalker" , "Tarkovsky", 1980);
+        DVD dv2 = new DVD("LotR","Jeckson", 2001);
+        dvdarray[0]=dv2;
+        dvdarray[1] = dv1;
+
+        for(int i = 0; i < dvdarray.length; ++i){
+            System.out.println(dvdarray[i].toSring());
+        }
+
         dv1.toSring();
     }
 
