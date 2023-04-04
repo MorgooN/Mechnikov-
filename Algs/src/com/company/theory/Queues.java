60ef7402634b31ed8389832068275f68d0e930d6
morgu <morgunov.96@bk.ru>
2023-04-04 23:57:30 +0300

    Circular style

--- Algs/src/com/company/theory/Queues.java
+++ Algs/src/com/company/theory/Queues.java
@@ -32,8 +32,83 @@ class MyQueue{
     }
 }
 
+class MyCircularQueue {
+
+    private int[] data;
+    private int head;
+    private int tail;
+    private int size;
+
+
+    public MyCircularQueue(int k) {
+        data = new int[k];
+        head = -1;
+        tail = -1;
+        size = k;
+    }
+
+
+    public boolean enQueue(int value) {
+        if (isFull() == true) {
+            return false;
+        }
+        if (isEmpty() == true) {
+            head = 0;
+        }
+        tail = (tail + 1) % size;
+        data[tail] = value;
+        return true;
+    }
+
+
+    public boolean deQueue() {
+        if (isEmpty() == true) {
+            return false;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
+        }
+        head = (head + 1) % size;
+        return true;
+    }
+
+
+    public int Front() {
+        if (isEmpty() == true) {
+            return -1;
+        }
+        return data[head];
+    }
+
+
+    public int Rear() {
+        if (isEmpty() == true) {
+            return -1;
+        }
+        return data[tail];
+    }
+
+
+    public boolean isEmpty() {
+        return head == -1;
+    }
+
+
+    public boolean isFull() {
+        return ((tail + 1) % size) == head;
+    }
+}
+
+
+
 public class Queues {
     public static void main(String args[]){
+
+        Queue<Integer> q = new LinkedList<>();
+
+        /*
         MyQueue q1 = new MyQueue();
         q1.enQueue(7);
         q1.enQueue(38);
@@ -57,5 +132,7 @@ public class Queues {
         else{
             System.out.println("Queue is empty");
         }
+
+         */
     }
 }
