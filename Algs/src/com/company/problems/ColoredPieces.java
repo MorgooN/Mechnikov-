ccc9d517fbdde432dd9ffd7ab84633d74ff20b70
morgu <morgunov.96@bk.ru>
2023-10-02 15:57:00 +0300

    Forgot to add comments

--- Algs/src/com/company/problems/ColoredPieces.java
+++ Algs/src/com/company/problems/ColoredPieces.java
@@ -8,20 +8,23 @@ public class ColoredPieces {
        int aliceScore = 0;
        int bobScore = 0;
 
+       // iterating color without the edges and fixing colors
        for(int i = 1; i < color.length()-1; ++i){
            char currentColor = color.charAt(i);
            char prevColor = color.charAt(i-1);
            char nextColor = color.charAt(i+1);
 
-
+           // if alice can remove A then she got +1 score
            if(currentColor == 'A' & prevColor == 'A' & nextColor == 'A'){
                 aliceScore+=1;
            }
+           // same for bob
            else if(currentColor == 'B' & prevColor == 'B' & nextColor == 'B')
            {
                bobScore+=1;
            }
        }
+       // if true alice wins otherwise bob wins
        return aliceScore > bobScore;
     }
 
