1c0d8bb4eed9b7d8fe099111f55923af586370c7
morgu <morgunov.96@bk.ru>
2023-10-04 21:44:24 +0300

    HashMap designing

---
+++ Algs/src/com/company/problems/MyHashMap.java
@@ -0,0 +1,23 @@
+package com.company.problems;
+
+import java.util.Arrays;
+
+public class MyHashMap {
+        int[] map = new int[1000001];
+
+        MyHashMap(){
+            Arrays.fill(map,-1);
+        }
+
+        public void put(int key, int value){
+            map[key] = value;
+        }
+
+        public int get(int key){
+            return map[key];
+        }
+
+        public void remove(int key){
+            map[key] = -1;
+    }
+}
