dba7a1e8b16499e759704051c5ecbcc57983ab6f
morgu <morgunov.96@bk.ru>
2023-10-04 22:20:29 +0300

    More honest approach to the HashMap design

--- Algs/src/com/company/problems/MyHashMap.java
+++ Algs/src/com/company/problems/MyHashMap.java
@@ -1,6 +1,7 @@
 package com.company.problems;
 
 import java.util.Arrays;
+import java.util.*;
 
 public class MyHashMap {
         int[] map = new int[1000001];
@@ -19,5 +20,42 @@ public class MyHashMap {
 
         public void remove(int key){
             map[key] = -1;
+    }
+}
+/*
+Second approach which is more honest
+ */
+
+class MyHashMap2{
+
+    ArrayList<Integer> values = new ArrayList<>();
+    ArrayList<Integer> keys = new ArrayList<>();
+
+    public void put(int key, int value){
+        int i;
+        if((i = keys.indexOf(key))!=-1){
+            values.set(i, value);
+        }
+        else{ // if hashmap is empty - fill it
+            keys.add(key);
+            values.add(value);
+        }
+    }
+
+    public int get(int key){
+        int i;
+        if((i = keys.indexOf(key))!=-1) {
+            return values.get(i);
+        }
+            return -1;
+
+    }
+
+    public void remove(int key){
+        int i = keys.indexOf(key);
+        if(i !=-1){ // if at the end of map then remove
+            keys.remove(i);
+            values.remove(i);
+        }
     }
 }
